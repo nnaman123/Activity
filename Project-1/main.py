c248ca4384cd52efa69988413af237e794d06fc4
Namo18-web-sudo <nebula.foundation.official@gmail.com>
2025-10-07 20:23:09 +0530

    first commit

---
+++ Project-1/main.py
@@ -0,0 +1,12 @@
+import datetime
+
+today = datetime.date.today()
+now = datetime.datetime.now()
+print("Today's date:", today)
+print("Current time:", now)
+
+print("\n Date and Time Components: today.year =", today.year)
+print("today.month =", today.month)
+print("today.day =", today.day)
+print("now.hour =", now.hour)
+print("now.minute =", now.minute)  
